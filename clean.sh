97bffa9c75d3ef2c63124fb6092c482cc634d3af
K.K <gokennsirou@yahoo.co.jp>
2021-04-10 14:46:21 +0900

    clean scriptにエラー処理を追加

--- clean.sh
+++ clean.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-mv "./src/main.ts" "./archives/$1.ts"
+filename=$1
+
+if [ -z "$filename" ]; then
+  echo \"Error: no filename given\" && exit 1
+fi
+
+mv "./src/main.ts" "./archives/$filename.ts"
 cp "./template.ts" "./src/main.ts"
