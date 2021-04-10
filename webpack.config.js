2b92cc954f1b7320b40b1ba7b55b55712a6b0694
K.K <gokennsirou@yahoo.co.jp>
2021-04-10 19:43:25 +0900

    webpackの設定を修正、不要なpackageの削除

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
 module.exports = {
-    entry: path.join(__dirname, 'src', 'main.ts'),
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'main.js'
-    },
-    target: 'node',
-    module: {
-      rules: [{
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }]
-    }
+  entry: {
+    'main.dev': path.join(__dirname, 'src', 'main.dev.ts'),
+    'main.prod': path.join(__dirname, 'src', 'main.prod.ts')
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist')
+  },
+  target: 'node',
+  resolve: {
+    extensions: ['.ts']
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  }
 };
