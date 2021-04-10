0930bff5987760991f6440e18e13f26ba0e91447
K.K <gokennsirou@yahoo.co.jp>
2021-04-11 01:23:26 +0900

    memo.txtを追加

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -2,9 +2,10 @@
 
 readonly TEMPLATE_FILE_PATH='../.my-answer-template'
 readonly ANSWER_FILE_PATH='../src/my-answer.ts'
+readonly MEMO_FILE_PATH='../memo.txt'
 
 # 解答用テンプレートファイルをリセット
-function reset() {
+function reset_template() {
   local cwd="${1}"
   local template_file_path="${cwd}/${TEMPLATE_FILE_PATH}"
   local answer_file_path="${cwd}/${ANSWER_FILE_PATH}"
@@ -12,11 +13,19 @@ function reset() {
   cp "${template_file_path}" "${answer_file_path}"
 }
 
+# メモファイルをリセット
+function reset_memo() {
+  local cwd="${1}"
+  local memo_file_path="${cwd}/${MEMO_FILE_PATH}"
+
+  rm "${memo_file_path}" && touch "${memo_file_path}"
+}
+
 function main() {
   local cwd
   cwd="$(cd "$(dirname "$0")" && pwd)"
 
-  reset "${cwd}"
+  reset_template "${cwd}" && reset_memo "${cwd}"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
