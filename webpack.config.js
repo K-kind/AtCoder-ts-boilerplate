bd9b0e6718bdb95b1d8e8c9125473d41a17aa4ea
K.K <gokennsirou@yahoo.co.jp>
2021-04-10 18:15:21 +0900

    webpackでtsビルド、ファイルバンドルを行うように変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 module.exports = {
-    entry: path.join(__dirname, 'lib', 'main.js'),
+    entry: path.join(__dirname, 'src', 'main.ts'),
     output: {
       path: path.join(__dirname, 'dist'),
       filename: 'main.js'
     },
-    target: 'node'
+    target: 'node',
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }]
+    }
 };
